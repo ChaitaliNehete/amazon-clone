3aceceb1cb65c3209b95fb551dd1386ea418306c
ChaitaliNehete <cnehete23@gmail.com>
2021-11-09 11:24:03 +0530

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -52,7 +52,7 @@ const Login = () => {
 
                     <h5>Password</h5>
                     <input
-                        type="text"
+                        type="password"
                         value={password}
                         onChange={e => setPassword(e.target.value)}
                     />
